dac79dd398b1ab0ddd4640488444077faeab3c27
Grant Gonzalez <ggonzalez@Grants-M1-2021.lan>
2024-11-07 11:33:48 -0500

    Updated router to not redirect to /home

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,10 +9,6 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-			path: '/',
-			redirect: '/'
-		},
-		{
 			path: '/home',
 			name: 'home',
 			component: HomeView
